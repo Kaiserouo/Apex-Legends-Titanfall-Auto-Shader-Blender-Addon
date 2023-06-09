7fc35db53508c6c4b68926af677d81a0a503b5f8
Campbell Jones <git@serebit.com>
2023-06-09 19:59:50 -0400

    Change to using Apex_Shader_Plus1 instead of Cores

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 # builtin blender fine
-BUILTIN_BLENDER_FILE = str(Path(__file__).absolute().parent / 'asset' / 'Apex Shader.blend')
+BUILTIN_BLENDER_FILE = str(Path(__file__).absolute().parent / 'asset' / 'Apex_Shader_Plus1.blend')
 
 # blender shader file path, the default path is the one built-in inside addon 
 CORE_APEX_SHADER_BLENDER_FILE = BUILTIN_BLENDER_FILE

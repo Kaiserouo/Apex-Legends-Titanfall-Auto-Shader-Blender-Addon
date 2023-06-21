8124ccfa2d24c3483af129b01150183ea381c727
Campbell Jones <git@serebit.com>
2023-06-21 18:10:16 -0400

    Implement Plus node adder

--- config.py
+++ config.py
@@ -3,7 +3,9 @@
 from pathlib import Path
 
 # builtin blender fine
-BUILTIN_BLENDER_FILE = str(Path(__file__).absolute().parent / 'asset' / 'Apex_Shader_Plus1.blend')
+BUILTIN_BLENDER_FILE_BASE = str(Path(__file__).absolute().parent / 'asset' / 'Apex Shader.blend')
+BUILTIN_BLENDER_FILE_PLUS = str(Path(__file__).absolute().parent / 'asset' / 'Apex_Shader_Plus1.blend')
 
 # blender shader file path, the default path is the one built-in inside addon 
-CORE_APEX_SHADER_BLENDER_FILE = BUILTIN_BLENDER_FILE
+CORE_APEX_SHADER_BLENDER_FILE = BUILTIN_BLENDER_FILE_BASE
+PLUS_APEX_SHADER_BLENDER_FILE = BUILTIN_BLENDER_FILE_PLUS
